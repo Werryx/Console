b1c1e2d04fd72ab7703403f518c03bc416d5b618
Werryx <53448668+Werryx@users.noreply.github.com>
2020-05-14 23:15:37 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,3 +1,5 @@
+print("Поиск обновлений...")
+
 import requests
 import re
 
@@ -5,20 +7,33 @@ f = open(".version.txt")
 vernow = f.read()
 f.close()
 
-def update():
-	info = requests.get('https://github.com/Werryx/Console')
-	output = "".join(re.findall(r"</h(.*?)ml>", info.text))
+iv = requests.get('https://github.com/Werryx/Console/blob/master/.version.txt')
+ver = "".join(re.findall(r"<td id=\"LC1\" class=\"blob-code blob-code-inner js-file-line\">(.*?)</td>", iv.text))
 
-	f = open("main.py", "w")
+def update():
+	# Обновление файла .data
+	info = requests.get('https://github.com/Werryx/Console/blob/master/.data')
+	output = "".join(re.findall(r"<td id=\"LC1\" class=\"blob-code blob-code-inner js-file-line\">(.*?)</td>", info.text)).replace("\j", "\n").replace("&gt;", ">").replace("&quot;", "\"")
+	f = open(".data", "w")
 	f.write(output)
+	f.close()
+
+	# Обновление файла .version.txt
+	f = open(".version.txt", "w")
+	f.write(ver)
+	f.close()
+	vernow = ver
+
+	# Обновление файла main.py
+	info2 = requests.get('https://github.com/Werryx/Console/blob/master/main.py.txt')
+	output2 = "".join(re.findall(r"<td id=\"LC1\" class=\"blob-code blob-code-inner js-file-line\">(.*?)</td>", info2.text)).replace("\j", "\n").replace("\k", "\t").replace("&quot;", "\"")
+	f = open("main.py", "w")
+	f.write(output2)
 	f.close()
 
 	print("Файл обновлён до версии v." + vernow + " успешно")
 
-iv = requests.get('https://github.com/Werryx/Console/blob/master/.version.txt')
-ver = "".join(re.findall(r"<td id=\"LC1\" class=\"blob-code blob-code-inner js-file-line\">(.*?)</td>", iv.text))
-
-print(str(ver))
-
 if(vernow != ver):
 	update()
+else:
+	print("Вы используете последнюю версию")
